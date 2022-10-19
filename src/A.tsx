81fea1f138af73dc60518ce2189201afcd02a24a
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-10-19 00:49:35 -0300

    fazendo mais etapas

--- src/A.tsx
+++ src/A.tsx
@@ -1,13 +1,24 @@
 import { Component } from "react";
 import { B } from "./B";
 
-export class A extends Component{
+export class A extends Component {
+    private variableLength = [0, 1, 2, 4];
+
     render() {
         return (
-            <div style={{border: "1px solid red", padding: "10px", minWidth: "250px",maxWidth: "250px", minHeight: "250px", maxHeight: "250px"}}>
+            <div
+                style={{
+                    border: "1px solid red",
+                    padding: "10px",
+                    minWidth: "250px",
+                    maxWidth: "250px",
+                }}
+            >
                 <b>Componente A</b>
-                <B />
+                {this.variableLength.map((i) => {
+                    return <B />;
+                })}
             </div>
-        )
+        );
     }
-}   
+}
--- src/B.tsx
+++ src/B.tsx
@@ -1,24 +1,19 @@
 import { Component } from "react";
 import { ClickableContent } from "./ClickableContent";
 
-export class B extends Component{
-    constructor(props: any){
-        super(props);
-        this.state = {
-            count: 0
-        };
-    }
+export class B extends Component {
 
     render() {
         return (
             <div>
-                <div style={{border: "1px solid pink", padding: "10px"}}>
-                <button onClick={() => this.setState({count: this.state.count+1})}>Componente B {this.state.count}</button>
+                <div style={{ border: "1px solid pink", padding: "10px" }}>
+                    <div>Componente B</div>
+
+                    <ClickableContent />
+                </div>
             </div>
-                <ClickableContent />
-            </div>
-            
-            
-        )    
-    }   
+
+
+        )
+    }
 }   
